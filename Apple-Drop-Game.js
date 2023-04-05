013837fd02a7a4f539669d2984baf77438bfcb4e
StevenCude67 <124187339+StevenCude67@users.noreply.github.com>
2023-04-05 11:57:10 -0400

    Update Apple-Drop-Game.js

--- Apple-Drop-Game.js
+++ Apple-Drop-Game.js
@@ -1,12 +1,25 @@
-//This code creates a bucket that is sliding acorss the stone floor of a town, trying to catch apples falling fromthe sky
+//In this games, a god has decided the local town wastes too much food, so he rains apples down across the land. This being says "if a hero can step up and get "100 APPLE POINTS", then I'll stop the apple rain. So you stp up to the plate, seeing if you are worthy!
 
-//In question 1 I created the bucket, floor and clouds
+//Created by Steven Cude, used p5.js refrence for a few pieces of code
 
 function setup() {
+  //This makes the apples from a random X coordinate
   appleFallX = random(400);
   appleX = random(width);
+  //this starts and resets the apple to the top of the screen
   appleY = 0;
+  //I couldn't get the speed to be random everydrop, so I kept it at a nice even 6
+  appleSpeed = 6;
   appleFalling = false;
+  //Allows for manipulation of X and Y location of bucket
+  bucketX = 25;
+  bucketY = 325;
+  //Setting applePoints allows the code to keep track of your points
+  applePoints = 0;
+  //these variables let the color of the apple change each time, I added a 4th color, so it would actually cycle through the first three!
+  appleColorRandom = ["red", "yellow", "green", "orange"]
+  newColor = 2;
+  
   createCanvas(400, 400);
   ellipseMode(CORNER);
   angleMode(DEGREES);
@@ -21,14 +34,15 @@ function draw() {
   rect(0, 355, 400, 55);
 
   //draws apples that fall at random speeds
-  drawApple(appleX, appleY, 1);
-  if(appleFalling){
-    appleY = appleY + random(3, 8);
+  drawApple(appleX, appleY, 1, appleColorRandom[newColor]);
+  if (appleFalling) {
+    appleY = appleY + appleSpeed;
     
-    if(appleY >= 385){
-      appleFalling = false
-      appleY = 0
-      appleX = random(width)
+//restes apples to the top if they hit the bucket or miss it
+    if (appleY >= height - 60) {
+      appleFalling = false;
+      appleY = 0;
+      appleX = random(25, 375);
     }
   }
 
@@ -36,16 +50,52 @@ function draw() {
   drawClouds(-50, -80, 1);
 
   //draws bucket
-  drawBucket(mouseX - 25, 325, 1, "tan", "black");
+  drawBucket(mouseX - bucketX, bucketY, 1, "tan", "black");
 
-  //bucket glows gold when mouse is clicked
-  if (mouseIsPressed) {
-    drawBucket(mouseX - 25, 325, 1, "gold", "darkgoldenrod");
+  //shows "Apple Points:"
+  push();
+  fill("red");
+  strokeWeight(3);
+  textSize(20);
+  text("Apple Points:", 5, 25);
+  pop();
+  
+//Shows the amount of "Apple Points" the player has gained
+  push();
+  fill("red");
+  strokeWeight(3);
+  textSize(20);
+  text(applePoints, 130, 25);
+  pop();
+
+  //bucket glows gold when apple goes in bucket
+
+  if (dist(appleX, appleY, mouseX, 340) <= 40) {
+    drawBucket(mouseX - bucketX, bucketY, 1, "gold", "darkgoldenrod");
+    applePoints = applePoints + 1;
+  }
+//this part of the code makes the end screen appear if player get 100 "Apple Points"
+  if (applePoints === 100) {
+    fill("white");
+    rect(0, 0, 400);
+    fill("black");
+    strokeWeight(3);
+    textSize(40);
+    text("You Saved The Day!!", 10, 200);
+    noLoop();
+    drawApple(50, 250, 3, "red")
+    drawApple(170, 50, 3, "yellow")
+    drawApple(290, 250, 3, "green")
+  }
+  
+  //this resets the array to 0, so an error doesn't occur
+  if(newColor === 3){
+    newColor = 0
   }
 }
 
 //creates a bucket that can be moved on the X axis
-//can be moved and size changed when function is called
+//can be moved and scaled when it's called in draw()
 function drawBucket(bucketX, bucketY, bucketSize, bucketColor, bucketBarColor) {
   push();
   translate(bucketX, bucketY);
@@ -59,7 +109,7 @@ function drawBucket(bucketX, bucketY, bucketSize, bucketColor, bucketBarColor) {
 }
 
 //draws clouds on the top of the screen
-//can be moved and size changed when function is called
+//can be moved and scaled when it's called in draw()
 function drawClouds(cloudX, cloudY, cloudSize) {
   push();
   translate(cloudX, cloudY);
@@ -75,17 +125,21 @@ function drawClouds(cloudX, cloudY, cloudSize) {
   pop();
 }
 
-function drawApple(appleX, appleY, appleSize) {
+//draws apples that fall from the top of the screen
+//can be moved, scaled, and cahnged the color of the apple when it's called in draw()
+function drawApple(appleX, appleY, appleSize, appleColor){
   push();
   translate(appleX, appleY, appleSize);
   scale(appleSize);
-  fill("red");
+  fill(appleColor);
   ellipse(0, 0, 20);
-  fill("brown");
   rect(9, -4, 2, 4);
   pop();
 }
 
-function mousePressed(){
-  appleFalling = true
+//this function sets the appleFalling to true, so that the code on Line 38 can occur
+//this function also let's the array in line 20 to cycle through it's 4 indexes
+function mousePressed() {
+  appleFalling = true;
+  newColor = newColor + 1
 }
